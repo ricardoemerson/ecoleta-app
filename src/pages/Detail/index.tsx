d0a594d1ddc48847fbd40b91f261bec17e14b481
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2020-06-16 23:18:37 -0300

    Atualizada a leitura para o atributo de imagem.

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -25,6 +25,7 @@ interface Data {
     city: string;
     uf: string;
     image: string;
+    image_url: string;
   };
   items: {
     title: string;
@@ -72,7 +73,7 @@ const Detail: React.FC = () => {
           <Icon name="arrow-left" size={ 20 } color="#34cb79" />
         </TouchableOpacity>
 
-        <Image style={ styles.pointImage } source={{ uri: data.point.image }} />
+        <Image style={ styles.pointImage } source={{ uri: data.point.image_url }} />
 
         <Text style={ styles.pointName }>{ data.point.name }</Text>
         <Text style={ styles.pointItems }>{ data.items.map(item => item.title).join(', ') }</Text>
